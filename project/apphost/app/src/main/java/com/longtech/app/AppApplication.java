253b6f212ad0b66d15cb1021d6fd4840b1e92193
xiezhihua <xiezhihua@allinmd.cn>
2022-03-03 15:39:37 +0800

    [Android基础组件搭建] 源码BlockCanary-引入依赖（无法生效先放弃）

--- project/apphost/app/src/main/java/com/longtech/app/AppApplication.java
+++ project/apphost/app/src/main/java/com/longtech/app/AppApplication.java
@@ -1,8 +1,6 @@
 package com.longtech.app;
 import android.app.Application;
 
-import com.github.moduth.blockcanary.BlockCanary;
-import com.longtech.app.initialize.blockcanary.AppBlockCanaryContext;
 import com.longtech.services.ARouterManager;
 
 public class AppApplication extends Application {
