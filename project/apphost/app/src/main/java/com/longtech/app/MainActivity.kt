cd9f7a84722692d7ece2b0f6abb21ec31ea1aa61
xiezhihua <xiezhihua@allinmd.cn>
2022-03-08 16:44:14 +0800

    [Android基础组件搭建] 源码leakcanary引入

--- project/apphost/app/src/main/java/com/longtech/app/MainActivity.kt
+++ project/apphost/app/src/main/java/com/longtech/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.longtech.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Debug
 import android.util.Log
 import android.view.View
 import com.alibaba.android.arouter.launcher.ARouter
@@ -17,8 +18,6 @@ class MainActivity : AppCompatActivity() {
 
     fun onClick() {
         Log.d("CheckLoginAspect", "onClick")
-
-        Thread.sleep(5000L)
 
         //ARouter.getInstance().build("/okhttp/main").navigation(this)
         ARouter.getInstance().build("/retrofit2/main").navigation(this)
